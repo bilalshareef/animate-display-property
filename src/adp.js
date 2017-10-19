5e3af50510f9c936075760aeab60698713ceaf09
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2017-10-19 16:12:03 +0530

    Initial commit.

---
+++ src/adp.js
@@ -0,0 +1,54 @@
+var ADP = (function() {
+    
+    var isElementVisible = function(element) {
+        return (element.classList.contains('adp-hide')) ? false : true;
+    };
+    
+    var runCallback = function(callback) {
+        if(callback) {
+            callback();
+        }
+    };
+    
+    var show = function(element, animationName, callback) {
+        if(isElementVisible(element)) {
+            // The element is already showing up.
+            runCallback(callback);
+            return;
+        }
+        
+        var animationEndListener = function() {
+            element.classList.remove(animationName + '-adp-show');
+            runCallback(callback);
+            element.removeEventListener('animationend', animationEndListener);
+        };
+        
+        element.addEventListener('animationend', animationEndListener);
+        element.classList.remove('adp-hide');
+        element.classList.add(animationName + '-adp-show');
+    };
+    
+    var hide = function(element, animationName, callback) {
+        if(!isElementVisible(element)) {
+            // The element is hidden already.
+            runCallback(callback);
+            return;
+        }
+        
+        var animationEndListener = function() {
+            element.classList.add('adp-hide');
+            element.classList.remove(animationName + '-adp-hide');
+            runCallback(callback);
+            element.removeEventListener('animationend', animationEndListener);
+        };
+        
+        element.addEventListener('animationend', animationEndListener);
+        element.classList.add(animationName + '-adp-hide');
+    };
+    
+    return {
+        show: show,
+        hide: hide
+    };
+    
+}());
