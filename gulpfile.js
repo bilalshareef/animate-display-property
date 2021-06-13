3c224d03156bf6a5d8ecc02e766fe899c82f131b
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2021-06-13 23:35:44 +0530

    Added logic to cancel currently running animation if show is invoked when hide is being performed or vice versa. Also, improved build scripts.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,66 @@
-var gulp = require('gulp');
-var del = require('del');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var del = require("del");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var uglifycss = require("gulp-uglifycss");
+var rename = require("gulp-rename");
 
 var config = {
-    buildFolder: 'build',
+    distFolder: "dist"
 };
 
-gulp.task('clean', function() {
-    del([config.buildFolder + '/*']);
+gulp.task("clean", function() {
+    del([config.distFolder]);
 });
 
-gulp.task('copy', function() {
+gulp.task("copy-adp-hide", function() {
     return gulp.src([
-        'src/**/*'
-    ]).pipe(gulp.dest(config.buildFolder));
+        "src/adp-hide.css"
+    ]).pipe(gulp.dest(config.distFolder));
 });
 
-gulp.task('minify-js', ['copy'], function() {
-    return gulp.src(config.buildFolder + '/adp.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(config.buildFolder));
+gulp.task("copy-effects", function () {
+    return gulp.src([
+        "src/effects/**/*"
+    ]).pipe(gulp.dest(config.distFolder + "/effects"));
 });
 
-gulp.task('minify-css', ['minify-js'], function() {
-    return gulp.src(config.buildFolder + '/adp.css')
+gulp.task("copy-js", function() {
+    return gulp.src([
+        "src/adp.js"
+    ]).pipe(gulp.dest(config.distFolder));
+});
+
+gulp.task("copy", ["copy-adp-hide", "copy-effects", "copy-js"]);
+
+gulp.task("concat-css", function () {
+    return gulp.src([
+        "src/effects/*",
+        "src/adp-hide.css"
+    ]).pipe(concat("adp.css"))
+    .pipe(gulp.dest(config.distFolder));
+});
+
+gulp.task("minify-effects", function() {
+    return gulp.src([
+        "src/effects/**/*"
+    ]).pipe(uglifycss())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(config.distFolder + "/effects"));
+});
+
+gulp.task("minify-css", function() {
+    return gulp.src([config.distFolder + "/adp.css", config.distFolder + "/adp-hide.css"])
         .pipe(uglifycss())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest(config.buildFolder));
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest(config.distFolder));
 });
 
-gulp.task('build', ['minify-css']);
+gulp.task("minify-js", function() {
+    return gulp.src(config.distFolder + "/adp.js")
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest(config.distFolder));
+});
+
+gulp.task("minify", ["minify-effects", "minify-css", "minify-js"]);
--- src/adp.js
+++ src/adp.js
@@ -1,59 +1,163 @@
 var ADP = (function () {
-    
+
+    var animationStack = [];
+
+    var pushToStack = function (animationDetails) {
+        animationStack.push(animationDetails);
+    };
+
+    var removeFromStack = function (element) {
+        var index = -1;
+
+        for (var i = 0; i < animationStack.length; i++) {
+            if (animationStack[i].element === element) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index > -1) {
+            animationStack.splice(index, 1);
+        }
+    };
+
+    var inStack = function (element, animationType) {
+        var inStack = false;
+
+        for (var i = 0; i < animationStack.length; i++) {
+            if (animationStack[i].element === element && animationStack[i].animationType === animationType) {
+                inStack = true;
+                break;
+            }
+        }
+
+        return inStack;
+    };
+
+    var getFromStack = function (element) {
+        var animation = false;
+
+        for (var i = 0; i < animationStack.length; i++) {
+            if (animationStack[i].element === element) {
+                animation = animationStack[i];
+                break;
+            }
+        }
+
+        return animation;
+    };
+
     var isElementVisible = function (element) {
         return (element.classList.contains('adp-hide')) ? false : true;
     };
-    
+
     var runCallback = function (callback) {
         if (callback) {
             callback();
         }
     };
-    
-    var show = function (element, animationName, callback) {
+
+    var cancelAnimation = function (element, nextAnimationType) {
+        // The last animation is still running. Cancel It.
+        // Get currently running animation details.
+        var currentAnimation = getFromStack(element);
+
+        element.classList.remove(currentAnimation.effect + '-adp-show');
+        element.classList.remove(currentAnimation.effect + '-adp-hide');
+        removeFromStack(element);
+
+        if (nextAnimationType == 'show') {
+            element.classList.add('adp-hide');
+        }
+    };
+
+    var show = function (element, effect, callback) {
+        if (inStack(element, 'show')) {
+            // The element currently being animated to show. So do nothing.
+            return;
+        }
+
+        if (inStack(element, 'hide')) {
+            // The element currently being animated to hide. Cancel hide animation and proceed with show.
+            cancelAnimation(element, 'show');
+        }
+
         if (isElementVisible(element)) {
             // The element is already showing up.
             runCallback(callback);
             return;
         }
-        
+
         var animationEndListener = function () {
-            element.classList.remove(animationName + '-adp-show');
-            runCallback(callback);
+            if (inStack(element, 'show')) {
+                // The animation hasn't been canceled.
+                removeFromStack(element);
+                element.classList.remove(effect + '-adp-show');
+                runCallback(callback);
+            }
             element.removeEventListener(animationEndEvent, animationEndListener);
         };
-        
+
         element.addEventListener(animationEndEvent, animationEndListener);
         element.classList.remove('adp-hide');
-        element.classList.add(animationName + '-adp-show');
+        element.classList.add(effect + '-adp-show');
+        pushToStack({
+            element: element,
+            effect: effect,
+            animationType: 'show'
+        });
     };
-    
-    var hide = function (element, animationName, callback) {
+
+    var hide = function (element, effect, callback) {
+        if (inStack(element, 'hide')) {
+            // The element currently being animated to hide. So do nothing.
+            return;
+        }
+
+        if (inStack(element, 'show')) {
+            // The element currently being animated to show. Cancel show animation and proceed with hide.
+            cancelAnimation(element, 'hide');
+        }
+
         if (!isElementVisible(element)) {
             // The element is hidden already.
             runCallback(callback);
             return;
         }
-        
+
         var animationEndListener = function () {
-            element.classList.add('adp-hide');
-            element.classList.remove(animationName + '-adp-hide');
-            runCallback(callback);
+            if (inStack(element, 'hide')) {
+                // The animation hasn't been canceled.
+                removeFromStack(element);
+                element.classList.add('adp-hide');
+                element.classList.remove(effect + '-adp-hide');
+                runCallback(callback);
+            }
             element.removeEventListener(animationEndEvent, animationEndListener);
         };
-        
+
         element.addEventListener(animationEndEvent, animationEndListener);
-        element.classList.add(animationName + '-adp-hide');
+        element.classList.add(effect + '-adp-hide');
+        pushToStack({
+            element: element,
+            effect: effect,
+            animationType: 'hide'
+        });
     };
-    
-    var toggle = function (element, animationName, callback) {
+
+    var toggle = function (element, effect, callback) {
+        if (inStack(element, 'show') || inStack(element, 'hide')) {
+            // The element is currently being animated. So do nothing.
+            return;
+        }
+
         if (isElementVisible(element)) {
-            hide(element, animationName, callback);
+            hide(element, effect, callback);
         } else {
-            show(element, animationName, callback);
+            show(element, effect, callback);
         }
     };
-    
+
     var getEventName = function () {
         var element = document.createElement('div');
         if (element.style.webkitAnimation) {
@@ -64,13 +168,13 @@ var ADP = (function () {
             return 'animationend';
         }
     };
-    
+
     var animationEndEvent = getEventName();
-    
+
     return {
         show: show,
         hide: hide,
         toggle: toggle
     };
-    
+
 }());
