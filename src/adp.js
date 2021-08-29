f0937b928a6325173f4aaafec0dcc8c1e477cd2f
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2021-08-29 20:32:39 +0530

    Added comments in adp.js file.

--- src/adp.js
+++ src/adp.js
@@ -1,5 +1,9 @@
 var ADP = (function () {
 
+    /**
+     * State representing what happened to the element when show/hide was called.
+     * @type {Object}
+     */
     var states = {
         ANIMATION_COMPLETE: "animation-complete",
         ALREADY_VISIBLE: "already-visible",
@@ -9,12 +13,24 @@ var ADP = (function () {
         ANIMATION_CANCELLED: "animation-cancelled"
     };
 
+    /**
+     * Animation stack to maintain active animations.
+     * @type {Array<Object>}
+     */
     var animationStack = [];
 
+    /**
+     * Function to add an active animation to animation stack.
+     * @param {Object} animationDetails Animation details consisting of element, effect, callback and animationType.
+     */
     function pushToStack(animationDetails) {
         animationStack.push(animationDetails);
     }
 
+    /**
+     * Function to remove an inactive animation from animation stack.
+     * @param {Object} element DOM element whose animation data to be removed from animation stack.
+     */
     function removeFromStack(element) {
         var index = -1;
 
@@ -30,6 +46,12 @@ var ADP = (function () {
         }
     }
 
+    /**
+     * Function to check if an element with specific animation type is present in the animation stack or not.
+     * @param {Object} element DOM element which is being checked if present in animation stack.
+     * @param {string} animationType Either show or hide.
+     * @returns {boolean} Returns true if element present in stack and false otherwise.
+     */
     function inStack(element, animationType) {
         var inStack = false;
 
@@ -43,6 +65,11 @@ var ADP = (function () {
         return inStack;
     }
 
+    /**
+     * Function to get active animation of an element from animation stack.
+     * @param {Object} element DOM element whose animation details are being fetched.
+     * @returns {Object|boolean} Animation details for the passed element. If animation not present in stack, returns false.
+     */
     function getFromStack(element) {
         var animation = false;
 
@@ -56,16 +83,31 @@ var ADP = (function () {
         return animation;
     }
 
+    /**
+     * Function to check if a DOM element is visible or not.
+     * @param {Object} element DOM element which is being checked if visible or not.
+     * @returns {boolean} Returns true if element is visible and false otherwise.
+     */
     function isElementVisible(element) {
         return (element.classList.contains("adp-hide")) ? false : true;
     }
 
+    /**
+     * Function to run callback(if present) which indicates end of show or hide operation.
+     * @param {function} callback Callback function which is passed by the caller of show, hide or toggle methods.
+     * @param {string} state State indicating what happened to the element when show/hide was called.
+     */
     function runCallback(callback, state) {
         if (callback) {
             callback(state);
         }
     }
 
+    /**
+     * Function to cancel currently running animation.
+     * @param {Object} element DOM element whose animation needs to be cancelled.
+     * @param {string} nextAnimationType Either show or hide.
+     */
     function cancelAnimation(element, nextAnimationType) {
         // The last animation is still running. Cancel It.
         // Get currently running animation details.
@@ -81,6 +123,13 @@ var ADP = (function () {
         }
     }
 
+    /**
+     * Function to show an element with animation.
+     * @param {Object} element DOM element which needs to be shown.
+     * @param {string} effect The animation effect to be applied while showing the element.
+     * @param {function} callback Callback function which will be invoked once the show operation is completed.
+     * @returns void
+     */
     function show(element, effect, callback) {
         if (inStack(element, "show")) {
             // The element is currently being animated to show. So do nothing.
@@ -99,6 +148,10 @@ var ADP = (function () {
             return;
         }
 
+        /**
+         * Event listener function for animation end.
+         * If the element is in stack then animation is successfully completed.
+         */
         function animationEndListener() {
             if (inStack(element, "show")) {
                 // The animation hasn't been canceled.
@@ -120,6 +173,13 @@ var ADP = (function () {
         });
     }
 
+    /**
+     * Function to hide an element with animation.
+     * @param {Object} element DOM element which needs to be hidden.
+     * @param {string} effect The animation effect to be applied while hiding the element.
+     * @param {function} callback Callback function which will be invoked once the hide operation is completed.
+     * @returns void
+     */
     function hide(element, effect, callback) {
         if (inStack(element, "hide")) {
             // The element is currently being animated to hide. So do nothing.
@@ -138,6 +198,10 @@ var ADP = (function () {
             return;
         }
 
+        /**
+         * Event listener function for animation end.
+         * If the element is in stack then animation is successfully completed.
+         */
         function animationEndListener() {
             if (inStack(element, "hide")) {
                 // The animation hasn't been canceled.
@@ -159,6 +223,13 @@ var ADP = (function () {
         });
     }
 
+    /**
+     * Function to toggle an element between show and hide.
+     * @param {Object} element DOM element which needs to be shown/hidden.
+     * @param {string} effect The animation effect to be applied while showing/hiding the element.
+     * @param {function} callback Callback function which will be invoked once the show/hide operation is completed.
+     * @returns void
+     */
     function toggle(element, effect, callback) {
         if (inStack(element, "show")) {
             // The element is currently being animated to show. So perform hide.
@@ -178,6 +249,10 @@ var ADP = (function () {
         }
     }
 
+    /**
+     * Function to get proper animation end event name so that older versions of browsers are also supported.
+     * @returns {string} Animation end event name.
+     */
     function getEventName() {
         var element = document.createElement("div");
         if (element.style.webkitAnimation) {
@@ -189,6 +264,10 @@ var ADP = (function () {
         }
     }
 
+    /**
+     * Animation end event name.
+     * @type {string}
+     */
     var animationEndEvent = getEventName();
 
     return {
